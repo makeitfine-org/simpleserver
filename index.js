d1feecdcc4e8c81b95f17a3bde1460eeb9639e90
stingion <stingion@gmail.com>
2023-09-30 10:08:47 +0300

    Init

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/greet', (req, res) => {
+    res.send('Hi!')
+})
+
+app.get('/greet', (req, res) => {
+    res.send('Hi!')
+})
+
+app.post('/greet', (req, res) => {
+    res.send('POST: Hi!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
